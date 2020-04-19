7dd00dc2e3c64eef4eb3b6cadae129ae2bb7bc34
Graham Breed <gbreed@gmail.com>
2020-04-19 01:01:43 +0100

    one-line C imports

--- festamp.zig
+++ festamp.zig
@@ -1,13 +1,11 @@
-const c = @cImport({
-    @cInclude("stdio.h");
-    @cInclude("time.h");
-});
+const time = @cImport(@cInclude("time.h"));
+const stdio = @cImport(@cInclude("stdio.h"));
 
 pub fn main() void {
-    var timestamp: c.timespec = undefined;
-    var local: c.tm = undefined;
-    _ = c.clock_gettime(c.CLOCK_REALTIME, &timestamp);
-    _ = c.localtime_r(&timestamp.tv_sec, &local);
+    var timestamp: time.timespec = undefined;
+    var local: time.tm = undefined;
+    _ = time.clock_gettime(time.CLOCK_REALTIME, &timestamp);
+    _ = time.localtime_r(&timestamp.tv_sec, &local);
     const year = local.tm_year + 1900;
     const month = local.tm_mon;
     const quarter = @mod(year, 1024) * 4 + @divFloor(month, 3);
@@ -16,12 +14,12 @@ pub fn main() void {
       qday += 1;
     }
     const week = @divFloor(qday + local.tm_mday + 5 - local.tm_wday, 7);
-    _ = c.printf(c"%03x%01x", quarter, week);
+    _ = stdio.printf(c"%03x%01x", quarter, week);
     var halfday = local.tm_wday * 2;
     if (local.tm_hour > 11) {
         halfday += 1;
     }
     var ticks = (local.tm_min * 4 + @divFloor(local.tm_sec, 15));
     ticks = @divFloor(ticks * 16,  15);
-    _ = c.printf(c".%01x%01x%01x\n", halfday, local.tm_hour, ticks);
+    _ = stdio.printf(c".%01x%01x%01x\n", halfday, local.tm_hour, ticks);
 }
