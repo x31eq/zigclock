7dd77a02b9372501c77bb97990949731333b964f
Graham Breed <gbreed@gmail.com>
2020-04-20 12:53:21 +0100

    Decode the time as part of feestamp

--- feetime.zig
+++ feetime.zig
@@ -36,3 +36,52 @@ pub fn currentTime() Time {
         .sec = @truncate(u8, sec),
     };
 }
+
+const MuggleTime = packed struct {
+    year: i32,
+    month: u8,
+    day: u8,
+    hour: u8,
+    min: u8,
+    sec: u8,
+};
+
+pub fn decode(feetime: Time) MuggleTime {
+    const year = 1920 + @mod(@divFloor(feetime.quarter, 4) + 128, 1024);
+    const month = (@intCast(u8, feetime.quarter) % 4) * 3
+                    + feetime.week / 0x55;
+    var k = (month % 3) * 38 + 5;
+    if (month ==  2 or month == 11) {
+        k -= 1;
+    }
+    const day = feetime.week * 7 + feetime.halfday / 2
+            + (1 + k - month_weekday(year, month)) % 7 - k;
+    const toc = feetime.tick / 16 * 15 + feetime.tick % 16;
+    return MuggleTime {
+        .year = year,
+        .month = month + 1,
+        .day = day,
+        .hour = feetime.hour + 12 * (feetime.halfday & 1),
+        .min = toc / 4,
+        .sec = (toc % 4) * 15 + feetime.sec,
+    };
+}
+
+/// Weekday (Sunday is 0) of the first day of the month
+/// month is 0 for January
+fn month_weekday(year: i32, month: u8) u8 {
+    // Based on RFC 3339 Appendix B
+    var Y = year;
+
+    var m = @intCast(i32, month) - 1;
+    if (m < 1) {
+        m += 12;
+        Y -= 1;
+    }
+    const cent = @divFloor(Y, 100);
+    Y = @mod(Y, 100);
+
+    const day = @divFloor(26 * m - 2, 10) + 1 + Y
+                + @divFloor(Y, 4) + @divFloor(cent, 4) + 5 * cent;
+    return @intCast(u8, @mod(day, 7));
+}
