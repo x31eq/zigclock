01f9dc3fbabbc75b8844ec977f0f4eea86b1ee75
Graham Breed <gbreed@gmail.com>
2020-04-22 13:51:39 +0100

    Read the command line argument

--- fedate.zig
+++ fedate.zig
@@ -2,8 +2,11 @@ const std = @import("std");
 const feetime = @import("feetime.zig");
 
 pub fn main() !void {
-    // Constant until I work out command line arguments
-    const stamp = "f913.6b66";
+    const stdout = try std.io.getStdOut();
+    if (std.os.argv.len < 2) {
+        try stdout.write("Supply the hex timestamp on the command line");
+    }
+    const stamp = std.os.argv[1];
     var quarter = try std.fmt.parseInt(i24, stamp[0..3], 16);
     if (quarter < 0xe00) {
         quarter += 0x2000;
@@ -19,7 +22,6 @@ pub fn main() !void {
         .tick = try std.fmt.parseInt(u8, stamp[7..9], 16),
     };
     const muggle = feetime.decode(instant);
-    const stdout = try std.io.getStdOut();
     var mugglebuf = "YYYY-mm-dd HH:MM:SS\n";
     _ = try std.fmt.bufPrint(mugglebuf[0..],
             "{}-{d:0>2}-{d:0>2} {d:0>2}:{d:0>2}:{d:0>2}",
