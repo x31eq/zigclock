90a3eeecca0e0d1f0287681da19c7d0a993ea46c
Graham Breed <gbreed@gmail.com>
2020-04-22 20:47:47 +0100

    Allow times only in feedate

--- feedate.zig
+++ feedate.zig
@@ -8,17 +8,30 @@ pub fn main() !void {
         try stdout.write("Supply the hex timestamp on the command line\n");
         return;
     }
+    var instant: feetime.Time = undefined;
     const stamp = std.os.argv[1];
-    var quarter = try fmt.parseInt(i24, stamp[0..2], 16);
-
-    const instant = feetime.Time {
-        .quarter = quarter + 0x1f00,
-        .week = try fmt.charToDigit(stamp[2], 16),
-        .halfday = try fmt.charToDigit(stamp[3], 16),
-        .hour = try fmt.charToDigit(stamp[5], 16),
-        .tick = try fmt.parseInt(u8, stamp[6..8], 16),
-        .sec = try fmt.charToDigit(stamp[8], 16),
-    };
+    if (stamp[0] == ':') {
+        // time with no date
+        instant = feetime.Time {
+            .quarter = 1984 * 4,
+            .week = 0,
+            .halfday = 0,
+            .hour = try fmt.charToDigit(stamp[1], 16),
+            .tick = try fmt.parseInt(u8, stamp[2..4], 16),
+            .sec = try fmt.charToDigit(stamp[4], 16),
+        };
+    }
+    else {
+        var quarter = try fmt.parseInt(i24, stamp[0..2], 16);
+        instant = feetime.Time {
+            .quarter = quarter + 0x1f00,
+            .week = try fmt.charToDigit(stamp[2], 16),
+            .halfday = try fmt.charToDigit(stamp[3], 16),
+            .hour = try fmt.charToDigit(stamp[5], 16),
+            .tick = try fmt.parseInt(u8, stamp[6..8], 16),
+            .sec = try fmt.charToDigit(stamp[8], 16),
+        };
+    }
     const muggle = feetime.decode(instant);
     var mugglebuf = "YYYY-mm-dd HH:MM:SS\n";
     _ = try fmt.bufPrint(mugglebuf[0..],
