91e4994179a7b625dca231dea70ace64a3c38ed0
Graham Breed <gbreed@gmail.com>
2020-04-22 10:54:44 +0100

    Treat quarter and week separately in fedate
    The old way of doing it was because I couldn't format
    3 hex digits at the time.
    
    Note that u12 is a valid type in Zig, but it doesn't really help here.

--- festamp.zig
+++ festamp.zig
@@ -3,11 +3,11 @@ const feetime = @import("feetime.zig");
 
 pub fn main() !void {
     const now = feetime.currentTime();
-    const date = @intCast(u32, now.quarter) * 16 + now.week;
+    const quarter = @intCast(u16, now.quarter & 0xfff);
     const stdout = try std.io.getStdOut();
     var arrbuf = "xxxx.xxxx\n";
     _ = try std.fmt.bufPrint(arrbuf[0..],
-            "{x:0>4}.{x:0>1}{x:0>1}{x:0>2}",
-            @truncate(u16, date), now.halfday, now.hour, now.tick);
+            "{x:0>3}{x:0>1}.{x:0>1}{x:0>1}{x:0>2}",
+            quarter, now.week, now.halfday, now.hour, now.tick);
     try stdout.write(arrbuf);
 }
