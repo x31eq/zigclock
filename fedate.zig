3f3feb7b520dd9f81def37186338d799dbe043d1
Graham Breed <gbreed@gmail.com>
2020-04-22 15:45:59 +0100

    assign std.fmt

--- fedate.zig
+++ fedate.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const feetime = @import("feetime.zig");
+const fmt = std.fmt;
 
 pub fn main() !void {
     const stdout = try std.io.getStdOut();
@@ -8,7 +9,7 @@ pub fn main() !void {
         return;
     }
     const stamp = std.os.argv[1];
-    var quarter = try std.fmt.parseInt(i24, stamp[0..3], 16);
+    var quarter = try fmt.parseInt(i24, stamp[0..3], 16);
     if (quarter < 0xe00) {
         quarter += 0x2000;
     } else {
@@ -17,14 +18,14 @@ pub fn main() !void {
 
     const instant = feetime.Time {
         .quarter = quarter,
-        .week = try std.fmt.charToDigit(stamp[3], 16),
-        .halfday = try std.fmt.charToDigit(stamp[5], 16),
-        .hour = try std.fmt.charToDigit(stamp[6], 16),
-        .tick = try std.fmt.parseInt(u8, stamp[7..9], 16),
+        .week = try fmt.charToDigit(stamp[3], 16),
+        .halfday = try fmt.charToDigit(stamp[5], 16),
+        .hour = try fmt.charToDigit(stamp[6], 16),
+        .tick = try fmt.parseInt(u8, stamp[7..9], 16),
     };
     const muggle = feetime.decode(instant);
     var mugglebuf = "YYYY-mm-dd HH:MM:SS\n";
-    _ = try std.fmt.bufPrint(mugglebuf[0..],
+    _ = try fmt.bufPrint(mugglebuf[0..],
             "{}-{d:0>2}-{d:0>2} {d:0>2}:{d:0>2}:{d:0>2}",
             muggle.year, muggle.month, muggle.day,
             muggle.hour, muggle.min, muggle.sec);
--- feedate.zig
+++ feedate.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const feetime = @import("feetime.zig");
+const fmt = std.fmt;
 
 pub fn main() !void {
     const stdout = try std.io.getStdOut();
@@ -8,19 +9,19 @@ pub fn main() !void {
         return;
     }
     const stamp = std.os.argv[1];
-    var quarter = try std.fmt.parseInt(i24, stamp[0..2], 16);
+    var quarter = try fmt.parseInt(i24, stamp[0..2], 16);
 
     const instant = feetime.Time {
         .quarter = quarter + 0x1f00,
-        .week = try std.fmt.charToDigit(stamp[2], 16),
-        .halfday = try std.fmt.charToDigit(stamp[3], 16),
-        .hour = try std.fmt.charToDigit(stamp[5], 16),
-        .tick = try std.fmt.parseInt(u8, stamp[6..8], 16),
-        .sec = try std.fmt.charToDigit(stamp[8], 16),
+        .week = try fmt.charToDigit(stamp[2], 16),
+        .halfday = try fmt.charToDigit(stamp[3], 16),
+        .hour = try fmt.charToDigit(stamp[5], 16),
+        .tick = try fmt.parseInt(u8, stamp[6..8], 16),
+        .sec = try fmt.charToDigit(stamp[8], 16),
     };
     const muggle = feetime.decode(instant);
     var mugglebuf = "YYYY-mm-dd HH:MM:SS\n";
-    _ = try std.fmt.bufPrint(mugglebuf[0..],
+    _ = try fmt.bufPrint(mugglebuf[0..],
             "{}-{d:0>2}-{d:0>2} {d:0>2}:{d:0>2}:{d:0>2}",
             muggle.year, muggle.month, muggle.day,
             muggle.hour, muggle.min, muggle.sec);
