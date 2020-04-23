a4d87221a7ad639b7170791d22f2c85331641ecf
Graham Breed <gbreed@gmail.com>
2020-04-23 15:12:39 +0100

    Decode a common string without dividers

--- decode.zig
+++ decode.zig
@@ -8,32 +8,25 @@ pub fn main() !void {
         try stdout.write("Supply the hex timestamp on the command line\n");
         return;
     }
+    var stamp = "00000.00000";
     const stamp_arg = std.os.argv[1];
     const stamp_in = stamp_arg[0..std.mem.len(u8, stamp_arg)];
     if (std.mem.indexOfScalar(u8, stamp_in, ':')) |_| {
-        var stamp = "000000:0000";
+        stamp = "000000:0000";
         try feetime.setStampFromArgs(stamp[0..], ':');
-        const instant = feetime.Time {
-            .quarter = try fmt.parseInt(i24, stamp[0..4], 16),
-            .week = try fmt.charToDigit(stamp[4], 16),
-            .halfday = try fmt.charToDigit(stamp[5], 16),
-            .hour = try fmt.charToDigit(stamp[7], 16),
-            .tick = try fmt.parseInt(u8, stamp[8..10], 16),
-            .sec = try fmt.charToDigit(stamp[10], 16),
-        };
-        try stdout.write(try feetime.isoFormat(instant));
+        std.mem.copy(u8, stamp[6..], stamp[7..]);
     }
     else {
-        var stamp = "00000.00000";
         try feetime.setStampFromArgs(stamp[0..], '.');
-        const instant = feetime.Time {
-            .quarter = try fmt.parseInt(i24, stamp[0..4], 16),
-            .week = try fmt.charToDigit(stamp[4], 16),
-            .halfday = try fmt.charToDigit(stamp[6], 16),
-            .hour = try fmt.charToDigit(stamp[7], 16),
-            .tick = try fmt.parseInt(u8, stamp[8..10], 16),
-            .sec = try fmt.charToDigit(stamp[10], 16),
-        };
-        try stdout.write(try feetime.isoFormat(instant));
+        std.mem.copy(u8, stamp[5..], stamp[6..]);
     }
+    const instant = feetime.Time {
+        .quarter = try fmt.parseInt(i24, stamp[0..4], 16),
+        .week = try fmt.charToDigit(stamp[4], 16),
+        .halfday = try fmt.charToDigit(stamp[6], 16),
+        .hour = try fmt.charToDigit(stamp[6], 16),
+        .tick = try fmt.parseInt(u8, stamp[7..9], 16),
+        .sec = try fmt.charToDigit(stamp[9], 16),
+    };
+    try stdout.write(try feetime.isoFormat(instant));
 }
