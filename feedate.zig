f2e432a9a1a08b2f88f1a52e7741fa4ae5f2f28e
Graham Breed <gbreed@gmail.com>
2020-04-23 13:14:52 +0100

    feedate can take exact quarters

--- feedate.zig
+++ feedate.zig
@@ -12,17 +12,28 @@ pub fn main() !void {
             i24, std.os.getenv("HEXEPOCH") orelse "1984", 10);
     const stamp_arg = std.os.argv[1];
     const stamp_in = stamp_arg[0..std.mem.len(u8, stamp_arg)];
-    var stamp = "0000:0000";
-    const offset = 4 - std.mem.indexOfScalar(u8, stamp_in, ':').?;
+    var stamp = "000000:0000";
+    const offset = 6 - std.mem.indexOfScalar(u8, stamp_in, ':').?;
     std.mem.copy(u8, stamp[offset..], stamp_in);
-    var quarter = try fmt.parseInt(i24, stamp[0..2], 16);
+    var quarter = try fmt.parseInt(i24, stamp[0..4], 16);
+    if (offset > 1) {
+        quarter += epoch * 4;
+    }
+    else if (offset > 0) {
+        if (quarter < 0xe00) {
+            quarter += 0x2000;
+        }
+        else {
+            quarter += 0x1000;
+        }
+    }
     const instant = feetime.Time {
-        .quarter = quarter + epoch * 4,
-        .week = try fmt.charToDigit(stamp[2], 16),
-        .halfday = try fmt.charToDigit(stamp[3], 16),
-        .hour = try fmt.charToDigit(stamp[5], 16),
-        .tick = try fmt.parseInt(u8, stamp[6..8], 16),
-        .sec = try fmt.charToDigit(stamp[8], 16),
+        .quarter = quarter,
+        .week = try fmt.charToDigit(stamp[4], 16),
+        .halfday = try fmt.charToDigit(stamp[5], 16),
+        .hour = try fmt.charToDigit(stamp[7], 16),
+        .tick = try fmt.parseInt(u8, stamp[8..10], 16),
+        .sec = try fmt.charToDigit(stamp[10], 18),
     };
     const muggle = feetime.decode(instant);
     var mugglebuf = "YYYY-mm-dd HH:MM:SS\n";
