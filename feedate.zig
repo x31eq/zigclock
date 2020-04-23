4595c03ac2d1aa0a2084a52d420431627b0c84a7
Graham Breed <gbreed@gmail.com>
2020-04-23 10:47:43 +0100

    More flexibility on reading feestamp

--- feedate.zig
+++ feedate.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const feetime = @import("feetime.zig");
 const fmt = std.fmt;
+const strlen = @cImport(@cInclude("string.h")).strlen;
 
 pub fn main() !void {
     const stdout = try std.io.getStdOut();
@@ -10,41 +11,30 @@ pub fn main() !void {
     }
     const epoch = try fmt.parseInt(
             i24, std.os.getenv("HEXEPOCH") orelse "1984", 10);
-    var instant: feetime.Time = undefined;
-    const stamp = std.os.argv[1];
-    if (stamp[0] == ':') {
-        // time with no date
-        instant = feetime.Time {
-            .quarter = epoch * 4,
-            .week = 0,
-            .halfday = 0,
-            .hour = try fmt.charToDigit(stamp[1], 16),
-            .tick = try fmt.parseInt(u8, stamp[2..4], 16),
-            .sec = try fmt.charToDigit(stamp[4], 16),
-        };
+    const stamp_arg = std.os.argv[1];
+    const arglen = strlen(stamp_arg);
+    var stamp = "0000:0000";
+    var offset: usize = 0;
+    if (stamp_arg[0] == ':') {
+        offset = 4;
     }
-    else if (stamp[1] == ':') {
+    else if (stamp_arg[1] == ':') {
         // only the half-day
-        instant = feetime.Time {
-            .quarter = epoch * 4,
-            .week = 1,
-            .halfday = try fmt.charToDigit(stamp[0], 16),
-            .hour = try fmt.charToDigit(stamp[2], 16),
-            .tick = try fmt.parseInt(u8, stamp[3..5], 16),
-            .sec = try fmt.charToDigit(stamp[5], 10),
-        };
+        offset = 3;
     }
-    else {
-        var quarter = try fmt.parseInt(i24, stamp[0..2], 16);
-        instant = feetime.Time {
-            .quarter = quarter + epoch * 4,
-            .week = try fmt.charToDigit(stamp[2], 16),
-            .halfday = try fmt.charToDigit(stamp[3], 16),
-            .hour = try fmt.charToDigit(stamp[5], 16),
-            .tick = try fmt.parseInt(u8, stamp[6..8], 16),
-            .sec = try fmt.charToDigit(stamp[8], 16),
-        };
+    for (stamp_arg[0..arglen]) |c| {
+        stamp[offset] = c;
+        offset += 1;
     }
+    var quarter = try fmt.parseInt(i24, stamp[0..2], 16);
+    const instant = feetime.Time {
+        .quarter = quarter + epoch * 4,
+        .week = try fmt.charToDigit(stamp[2], 16),
+        .halfday = try fmt.charToDigit(stamp[3], 16),
+        .hour = try fmt.charToDigit(stamp[5], 16),
+        .tick = try fmt.parseInt(u8, stamp[6..8], 16),
+        .sec = try fmt.charToDigit(stamp[8], 16),
+    };
     const muggle = feetime.decode(instant);
     var mugglebuf = "YYYY-mm-dd HH:MM:SS\n";
     _ = try fmt.bufPrint(mugglebuf[0..],
