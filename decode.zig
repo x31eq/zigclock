6f69633076578f2f0a90e412d82f593df0dd4619
Graham Breed <gbreed@gmail.com>
2020-04-25 10:36:04 +0100

    Make decode.zig delegate to the other main functions

--- decode.zig
+++ decode.zig
@@ -1,30 +1,14 @@
 const std = @import("std");
-const feetime = @import("feetime.zig");
 
 pub fn main() !u8 {
-    const stdout = try std.io.getStdOut();
     if (std.os.argv.len < 2) {
         std.debug.warn("Supply the hex timestamp on the command line\n");
         return 1;
     }
-    var stamp = "00000.00000";
     const stamp_arg = std.os.argv[1];
     const stamp_in = stamp_arg[0..std.mem.len(u8, stamp_arg)];
-    if (std.mem.indexOfScalar(u8, stamp_in, ':') != null) {
-        stamp = "000000:0000";
+    if (std.mem.indexOfScalar(u8, stamp_in, ':') == null) {
+        return try @import("fedate.zig").main();
     }
-    if (feetime.setStampFromArgs(stamp[0..])) {
-        if (feetime.timeFromHex(stamp)) |instant| {
-            var output_buffer = "YYYYY-mm-dd HH:MM:SS\n";
-            try stdout.write(try instant.isoFormat(output_buffer[0..]));
-            return 0;
-        }
-        else |_| {
-            std.debug.warn("Bad timestamp\n");
-        }
-    }
-    else |_| {
-        std.debug.warn("Bad epoch\n");
-    }
-    return 2;
+    return try @import("feedate.zig").main();
 }
